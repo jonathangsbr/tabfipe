d750dcfbaa71a327b45c9fd73bbdc7a55c966ba3
jonathangsbr <jonathan.spirandelli@unespar.edu.br>
2024-02-01 18:31:29 -0300

    Atualização no nome do projeto e na entity veiculo

--- entity/veiculo.go
+++ entity/veiculo.go
@@ -1,21 +1,10 @@
 package veiculo
 
 type Veiculo struct {
-	CodigoTabelaReferencia uint16
-	CodigoMarca            uint16
-	CodigoModelo           uint16
-	AnoModelo              uint16
-	CodigoTipoCombustivel  uint16
-	TipoVeiculo            string
-	ModeloCodigoExterno    string
-	TipoConsulta           string
-}
-
-type VeiculoResponse struct {
 	CodigoTabelaReferencia uint16 `json:"codigoTabelaReferencia"`
 	CodigoMarca            uint16 `json:"codigoMarca"`
 	CodigoModelo           uint16 `json:"codigoModelo"`
-	AnoModelo              uint16 `json:"anoModelo"`
+	AnoModelo              string `json:"anoModelo"`
 	CodigoTipoCombustivel  uint16 `json:"codigoTipoCombustivel"`
 	TipoVeiculo            string `json:"tipoVeiculo"`
 	ModeloCodigoExterno    string `json:"modeloCodigoExterno"`
